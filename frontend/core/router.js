5b8f0a8f6522d1c68feb6be1ca0822b4b076e549
kirikaya2006-pixel <kirikaya.2006@gmail.com>
2026-08-03 09:49:38 +0700

    R

--- frontend/core/router.js
+++ frontend/core/router.js
@@ -52,47 +52,46 @@ const Router = {
 
     },
 
-    async start(){
+    async start() {
 
         const path = window.location.pathname;
         const route = this.routes[path];
 
-        if(route){
-            if(route.auth){
-                const success = await Auth.check();
-
-                if(!success) {
-                    localStorage.clear();
-                    window.location.replace("/");
-                    return;
-                }
-            }
-
-            await loader.require(route.html, "#app");
-
-            if(route.css) {
-                route.css.forEach((css)=>{
-                    const link = document.createElement("link");
-
-                    link.rel = "stylesheet";
-                    link.href = loader.url(css);
-
-                    document.head.appendChild(link);
-                });
-            };
-            
-            if(route.js) {
-                route.js.forEach((js)=>{
-                    const script = document.createElement("script");
-
-                    script.src = loader.url(js);
-                    document.body.appendChild(script);
-                });
-            }
-        }else{
-            await loader.require("page/error/404.html","#app");
+        if (!route) {
+            await loader.require("page/error/404.html", "#app");
+            return;
         }
 
+        if (route.auth) {
+
+            const success = await Auth.check();
+
+            if (!success) {
+                localStorage.removeItem("token");
+                window.location.replace("/");
+                return;
+            }
+
+        }
+
+        await loader.require(route.html, "#app");
+
+        if (route.css) {
+            route.css.forEach((css) => {
+                const link = document.createElement("link");
+                link.rel = "stylesheet";
+                link.href = loader.url(css);
+                document.head.appendChild(link);
+            });
+        }
+
+        if (route.js) {
+            route.js.forEach((js) => {
+                const script = document.createElement("script");
+                script.src = loader.url(js);
+                document.body.appendChild(script);
+            });
+        }
     }
 
 }
